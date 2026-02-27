d9810085dc23a76e54cd8ab739dca3cedf0dabe9
Claudio Oliveira <claudio.oliveira.sants@gmail.com>
2026-02-26 21:55:49 -0300

    estrutura de app.jsx base pronto

--- multistep_form_react/src/App.jsx
+++ multistep_form_react/src/App.jsx
@@ -1,3 +1,6 @@
+// components 
+import {GrFormNext, GrFormPrevious} from 'react-icons/gr';
+
 
 import './App.css'
 
@@ -6,11 +9,35 @@ function App() {
 
   return (
     
-      <div className='App'> 
-        <h1>MultiStep Form</h1>
+      <div className='app'> 
+        <div className="header">
+          <h2>Deixe sua avaliação</h2>
+          <p>Ficamos felizes com sua compra, utilize o formulário abaixo para avaliar o produto.</p>
+        </div>
+        <div className="form-container">
+          <p>etapas</p>
+          <form>
+            {/* Formulário modificado */}
+            <div className="inputs-container">
+
+            </div>
+            {/* botões */}
+            <div className="actions">
+              <button type="button">
+                <GrFormPrevious/>
+                <span>Voltar</span>
+              </button>
+              <button type="submit">
+                <span>Avançar</span>
+                <GrFormNext/>
+              </button>
+
+            </div>
+          </form>
+        </div>
       </div>
      
-  )
+  );
 }
 
 export default App
